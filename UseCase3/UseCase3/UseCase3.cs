c0a6c6c98ed099f9ba02fb6ca6b922416c7bb5ca
MSRbuddy <meghanadasari222@gmail.com>
2023-04-01 16:07:12 +0530

    Added : Emp-Wage Prblm UC-3!

---
+++ UseCase3/UseCase3/UseCase3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UseCase3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PART_TIME = 1;
+            int FULL_TIME = 2;
+            int WAGE_PER_HOUR = 20;
+            
+            int workHrs = 0;
+            int Wage = 0;
+            Random random = new Random();
+
+            int number = random.Next(0, 3);
+            if(number==PART_TIME)
+            {
+                workHrs = 4;
+            }
+            else if(number==FULL_TIME)
+            {
+                workHrs = 8;
+            }
+            else
+            {
+                workHrs = 0;
+            }
+            Wage = workHrs * WAGE_PER_HOUR;
+            Console.WriteLine("Employee Wage is: " + Wage);
+        }
+    }
+}
