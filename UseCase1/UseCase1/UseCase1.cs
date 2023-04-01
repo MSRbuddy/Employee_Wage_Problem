bd7c14dfcf9956ee0d289426f1ef7ef5d5351398
MSRbuddy <meghanadasari222@gmail.com>
2023-04-01 16:03:42 +0530

    Added : Emp-Wage Prblm UC-1!

---
+++ UseCase1/UseCase1/UseCase1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UseCase1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int FULL_TIME = 1;
+            Random random = new Random();
+
+            int number = random.Next(0, 2);
+            if (number==FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
