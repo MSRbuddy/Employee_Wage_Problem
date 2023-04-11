6f1dda3279c0645ed1612af02e75956195ee1b8d
MSRbuddy <meghanadasari222@gmail.com>
2023-04-11 20:06:19 +0530

    Added: Emp-Wage prblm UC-10!

---
+++ UseCase10/UseCase10/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace UseCase10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            // Declaring Objects for companies
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("ABC", 20, 20, 100);
+            empWageBuilder.addCompanyEmpWage("XYZ", 40, 40, 70);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
