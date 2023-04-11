2e9dc311586d19c0584ec04552671665fed11df2
MSRbuddy <meghanadasari222@gmail.com>
2023-04-11 20:08:08 +0530

    Added: Emp-Wage prblm UC-11 n UC-12!

---
+++ UseCase11n12/UseCase11n12/CompanyEmployeeWage.cs
@@ -0,0 +1,30 @@
+﻿namespace UseCase11
+{
+    class CompanyEmployeeWage
+    {
+        public string company;
+        public int ratePerHours;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmployeeWage(string company, int ratePerHours, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.ratePerHours = ratePerHours;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            totalEmpWage = 0;
+        }
+
+        public void SettotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public string toString()
+        {
+            return "Total Emp Wage for company: " + company + " is " + totalEmpWage;
+        }
+    }
+}
---
+++ UseCase11n12/UseCase11n12/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UseCase11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            // Declaring Objects for companies
+            EmployeeWageBuilder empWageBuilder = new EmployeeWageBuilder();
+            empWageBuilder.AddCompanyEmpWage("ABC", 20, 2, 20);
+            empWageBuilder.AddCompanyEmpWage("XYZ", 15, 14, 40);
+            empWageBuilder.ComputeEmpWage();
+        }
+    }
+
+    public interface IComputeWage
+    {
+        void AddCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        void ComputeEmpWage();
+    }
+}
