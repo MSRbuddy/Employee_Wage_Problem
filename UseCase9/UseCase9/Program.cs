85e1d88c436873c21cce183ac199b44b3cec76ae
MSRbuddy <meghanadasari222@gmail.com>
2023-04-11 20:04:57 +0530

    Added: Emp-Wage prblm UC-9!

---
+++ UseCase9/UseCase9/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace UseCase9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program UC-9!");
+            // Declaring Objects for companies
+            EmployeeCompanyWageData abc = new EmployeeCompanyWageData("ABC", 20, 10, 50);
+            EmployeeCompanyWageData xyz = new EmployeeCompanyWageData("XYZ", 10, 30, 400);
+            // Calling the Objects
+            abc.calculateWage();
+            Console.WriteLine(abc.toString());
+            xyz.calculateWage();
+            Console.WriteLine(xyz.toString());
+        }
+    }
+}
